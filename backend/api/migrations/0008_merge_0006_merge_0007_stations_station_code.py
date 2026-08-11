99d74ecdfc667ced8088b1b22f543e2369d1f91a
Katya Vázquez <116518901+KatyaVazquez@users.noreply.github.com>
2026-08-11 14:35:55 -0300

    fix: format

--- backend/api/migrations/0008_merge_0006_merge_0007_stations_station_code.py
+++ backend/api/migrations/0008_merge_0006_merge_0007_stations_station_code.py
@@ -2,7 +2,6 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("api", "0006_merge_0005_seed_faq_0005_stationoverride"),
         ("api", "0007_stations_station_code"),
