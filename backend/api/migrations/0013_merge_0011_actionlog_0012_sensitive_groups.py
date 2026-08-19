86bb90bdc00396c2990ce083e0919013358c61cf
Katya Vázquez <116518901+KatyaVazquez@users.noreply.github.com>
2026-08-19 16:37:35 -0300

    fix format

--- backend/api/migrations/0013_merge_0011_actionlog_0012_sensitive_groups.py
+++ backend/api/migrations/0013_merge_0011_actionlog_0012_sensitive_groups.py
@@ -4,11 +4,9 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('api', '0011_institutionalert_actionlog'),
-        ('api', '0012_seed_sensitive_groups'),
+        ("api", "0011_institutionalert_actionlog"),
+        ("api", "0012_seed_sensitive_groups"),
     ]
 
-    operations = [
-    ]
+    operations = []
