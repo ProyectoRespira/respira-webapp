affa3fa49085acb6e7fd6c12b75e70cf537dc1c9
Katya Vázquez <116518901+KatyaVazquez@users.noreply.github.com>
2026-08-26 14:46:32 -0300

    fix; linter

--- backend/api/migrations/0014_merge_20260826_1744.py
+++ backend/api/migrations/0014_merge_20260826_1744.py
@@ -4,11 +4,9 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('api', '0013_devicefollower'),
-        ('api', '0013_merge_0011_actionlog_0012_sensitive_groups'),
+        ("api", "0013_devicefollower"),
+        ("api", "0013_merge_0011_actionlog_0012_sensitive_groups"),
     ]
 
-    operations = [
-    ]
+    operations = []
